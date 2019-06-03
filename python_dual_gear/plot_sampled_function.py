351ee5a4546667bc36ef76611eacd759348f9449
Fu Tianwen <kevin.futianwen@gmail.com>
2019-06-03 22:21:42 +0800

    tiny modifications

--- python_dual_gear/plot_sampled_function.py
+++ python_dual_gear/plot_sampled_function.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import numpy as np
 from math import sin, cos
+from functools import reduce
 
 
 def polar_to_rectangular(sample_function: [float], sample_points: [float]) -> [(float, float)]:
     assert len(sample_points) == len(sample_function)
     return [
-        (sample_function[i] * cos(sample_points[i]), sample_function[i] * sin(sample_points[i]))
-        for i in range(len(sample_points))
+        (r * cos(theta), r * sin(theta))
+        for r, theta in zip(sample_function, sample_points)
     ]
 
 
@@ -16,10 +17,10 @@ def plot_sampled_function(sample_functions: ([float]), range_start: float, range
     if sample_functions == ():
         return
 
+    assert reduce(lambda x, y: len(x) == len(y), sample_functions)
     fig, subplots = plt.subplots(1, len(sample_functions))
     sample_points = np.linspace(range_start, range_end, len(sample_functions[0]))
     for sample_function, subplot in zip(sample_functions, subplots):
-        assert len(sample_function) == len(sample_functions[0])
         polygon_points = polar_to_rectangular(sample_function, sample_points)
         # subplot.figure()
         subplot.fill([item[0] for item in polygon_points], [item[1] for item in polygon_points],
@@ -40,5 +41,5 @@ if __name__ == '__main__':
     import random
 
     drive_gear = [random.uniform(4, 5) for i in range(8192)]
-    y, L, phi = compute_dual_gear(drive_gear)
-    plot_sampled_function((drive_gear, y), 0, 2 * math.pi)
+    driven_gear, center_distance, phi = compute_dual_gear(drive_gear)
+    plot_sampled_function((drive_gear, driven_gear), 0, 2 * math.pi)
