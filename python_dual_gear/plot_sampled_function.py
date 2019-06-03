7e5262d6ca83a3111cd251dbfb0ef8cf66d56831
Fu Tianwen <kevin.futianwen@gmail.com>
2019-06-03 22:10:38 +0800

    revised plotting function

--- python_dual_gear/plot_sampled_function.py
+++ python_dual_gear/plot_sampled_function.py
@@ -1,13 +1,37 @@
 import matplotlib.pyplot as plt
+from matplotlib import animation
 import numpy as np
+from math import sin, cos
 
 
-def plot_sampled_function(sample_function: [float], range_start: float, range_end: float):
-    sample_points = np.linspace(range_start, range_end, len(sample_function))
-    plt.plot(sample_points, sample_function)
+def polar_to_rectangular(sample_function: [float], sample_points: [float]) -> [(float, float)]:
+    assert len(sample_points) == len(sample_function)
+    return [
+        (sample_function[i] * cos(sample_points[i]), sample_function[i] * sin(sample_points[i]))
+        for i in range(len(sample_points))
+    ]
+
+
+def plot_sampled_function(sample_functions: ([float]), range_start: float, range_end: float):
+    if sample_functions == ():
+        return
+
+    fig, subplots = plt.subplots(1, len(sample_functions))
+    sample_points = np.linspace(range_start, range_end, len(sample_functions[0]))
+    for sample_function, subplot in zip(sample_functions, subplots):
+        assert len(sample_function) == len(sample_functions[0])
+        polygon_points = polar_to_rectangular(sample_function, sample_points)
+        # subplot.figure()
+        subplot.fill([item[0] for item in polygon_points], [item[1] for item in polygon_points],
+                     facecolor='none', edgecolor='purple', linewidth=1)
+        subplot.axis('tight')
+        subplot.axis('equal')
+        subplot.axis('off')
     plt.show()
-    plt.polar(sample_points, sample_function)
-    plt.show()
+
+
+def rotate(sample_function: [float]) -> [float]:
+    pass
 
 
 if __name__ == '__main__':
@@ -16,6 +40,5 @@ if __name__ == '__main__':
     import random
 
     drive_gear = [random.uniform(4, 5) for i in range(8192)]
-    plot_sampled_function(drive_gear, 0, 2 * math.pi)
     y, L, phi = compute_dual_gear(drive_gear)
-    plot_sampled_function(y, 0, 2 * math.pi)
+    plot_sampled_function((drive_gear, y), 0, 2 * math.pi)
