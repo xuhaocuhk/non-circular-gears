e28dd12725ecb837f840efec4492534ea6b92be2
haoxu <hao.xu.chn@gmail.com>
2019-06-13 16:38:40 +0800

    实现在Euclidean coordinate 与 polar coordinate之间的完全转换

--- python_dual_gear/compute_shape_center.py
+++ python_dual_gear/compute_shape_center.py
@@ -14,14 +14,6 @@ def computeEuclideanCoord_x(r, theta):
 def computeEuclideanCoord_y(r, theta):
     return r*cos(theta)
 
-'''
-sample usage:
-polar_shape = [2, 1, 2, 1, 2, 1, 2, 1]
-x, y = toEuclideanCoord(polar_shape)
-'''
-def toEuclideanCoord(polar_r):
-    thetas = [theta*2*math.pi/len(polar_r) for theta in range(0, len(polar_r))]
-    return list( map(computeEuclideanCoord_x, polar_r, thetas )),  list( map(computeEuclideanCoord_y, polar_r, thetas ))
 
 def isAllVisible(p: Point, poly: Polygon):
     vtx = list(poly.exterior.coords)
@@ -30,6 +22,32 @@ def isAllVisible(p: Point, poly: Polygon):
         if line_center.crosses(polygon):
             return False
     return True
+
+'''
+sample usage:
+polar_shape = [2, 1, 2, 1, 2, 1, 2, 1]
+x, y = toEuclideanCoord(polar_shape)
+'''
+def toEuclideanCoord(polar_r):
+    thetas = [theta * 2 * math.pi / len(polar_r) for theta in range(0, len(polar_r))]
+    return list(map(computeEuclideanCoord_x, polar_r, thetas)), list(map(computeEuclideanCoord_y, polar_r, thetas))
+
+def getIntersDist(p:Point, theta, poly: Polygon, MAX_R):
+    outer_point = Point(p.x + MAX_R * sin(theta), p.y + MAX_R * cos(theta))
+    ring = LineString(list(poly.exterior.coords))
+    inters_pt = ring.intersection(LineString([p, outer_point]))
+    return p.distance(inters_pt)
+
+
+def toPolarCoord(p: Point, poly: Polygon, n: int):
+    assert isAllVisible(p, poly)
+    vtx = list(poly.exterior.coords)
+    distances = [p.distance(Point(v[0],v[1])) for v in vtx]
+    MAX_R = max(distances) + 10
+    sample_distances = [ getIntersDist(p, i * 2 * math.pi / n, poly, MAX_R) for i in range(n)]
+    return sample_distances
+
+
 
 # read coutour from a local file
 def getSVGShape(filename):
@@ -48,13 +66,17 @@ if __name__ == '__main__':
 
     plt.figure(figsize=(8, 8))
     plt.axis('equal')
-    plt.fill(x, y, "b", alpha = 0.3)
+    #plt.fill(x, y, "b", alpha = 0.3)
     for i in range(1000):
         x_i = (poly_bound[2] - poly_bound[0]) * np.random.random_sample() + poly_bound[0]
         y_i = (poly_bound[3] - poly_bound[1]) * np.random.random_sample() + poly_bound[1]
 
         if isAllVisible(Point(x_i, y_i), polygon):
             plt.scatter(x_i, y_i, s=50, c='b')
+            polar_poly = toPolarCoord(Point(x_i,y_i), polygon, 100)
+            new_x, new_y = toEuclideanCoord(polar_poly)
+            plt.fill(new_x, new_y, "r", alpha=0.3)
+            plt.show()
         else:
             plt.scatter(x_i, y_i, s=50, c='g')
 
