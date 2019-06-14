a57ac7942d72d51fb125768776618b5cc1dae4a0
Fu Tianwen <kevin.futianwen@gmail.com>
2019-06-14 22:19:12 +0800

    晚上写不动把自己写懵了 耻辱下播

--- python_dual_gear/compute_dual_gear.py
+++ python_dual_gear/compute_dual_gear.py
@@ -1,33 +1,54 @@
-from math import pi
+from math import pi, isclose
 import numpy as np
 
 
 def compute_dual_gear(x: [float], k: int = 1) -> ([float], float, [float]):
+    """
+    compute the dual gear with the gear given
+    :param x: sample points of drive gear's polar function, theta uniformly in [0, 2*pi)
+    :param k: drive gear runs k cycles in one cycle of the driven gear
+    :return: y, center_distance, phi
+    """
+    # calculate the center distance
     n = len(x)
-    target_function = lambda l: sum([xi / (l - xi) for xi in x]) / n - 1.0 / k
+    delta_alpha = 2 * pi / n
     iteration_bound = 50
     bound_left = max(x) * 1.001
     bound_right = max(x) * (k + 1.001)
+    target_final_phi = 2 * pi / k
+    float_tolerance = 1e-5  # constant
 
-    assert target_function(bound_left) * target_function(bound_right) < 0
+    # target function
+    def final_phi_bias(center_distance_estimate):
+        """
+        difference between the final phi and ideal final phi
+        """
+        final_phi = sum([delta_alpha * x_i / (center_distance_estimate - x_i) for x_i in x])
+        return final_phi - target_final_phi
+
+    # find the center distance
+    assert final_phi_bias(bound_left) * final_phi_bias(bound_right) < 0
     for i in range(iteration_bound):
         bound_middle = (bound_left + bound_right) / 2
-        if target_function(bound_middle) < 0:
+        if final_phi_bias(bound_middle) < 0:
             bound_right = bound_middle
         else:
             bound_left = bound_middle
     center_distance = (bound_left + bound_right) / 2
 
-    phi = [0.0] + list(map(lambda x: x * 2 * pi / n, cumulative_sum([xi / (center_distance - xi) for xi in x])))
-    normalize_factor = 1.0 / phi[-1] * 2 * pi
-    phi = [item * normalize_factor for item in phi]
-    t = np.linspace(0, 2 * pi, n + 1)
-    phiInv = np.interp(t, phi, t)  # inverse function of phi
-    y = [center_distance - xval for xval in np.interp(phiInv, t, x + [x[0]])]
-    y = y[:-1]
-    phi = [2 * pi - p for p in phi[::-1]]
-    phi = [-p for p in phi]
-    return y, center_distance, phi
+    # sum up to get phi
+    phi = cumulative_sum([delta_alpha * xi / (center_distance - xi) for xi in x])
+    assert isclose(phi[-1], target_final_phi, rel_tol=float_tolerance)
+    phi = [0] + phi[:-1]  # convert to our convention
+
+    # calculate the inverse function of phi
+    uniform_k_value_points = np.linspace(0, target_final_phi, n + 1, endpoint=True)  # final point for simplicity
+    phi_inv = np.interp(uniform_k_value_points, phi + [target_final_phi], np.linspace(0, 2 * pi, n + 1, endpoint=True))
+    assert isclose(phi_inv[0], 0, rel_tol=float_tolerance)
+    raise Exception("not finished!")
+
+    # duplicate for k times
+    pass
 
 
 def cumulative_sum(x: list) -> list:
@@ -36,3 +57,7 @@ def cumulative_sum(x: list) -> list:
     for i in range(1, length):
         result.append(result[i - 1] + x[i])
     return result
+
+
+if __name__ == '__main__':
+    compute_dual_gear([1, 2, 3, 4, 5])
