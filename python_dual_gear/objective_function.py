78870c2eeb5b7af9f738f2f660f0a95bad572801
Fu Tianwen <kevin.futianwen@gmail.com>
2019-07-19 21:29:54 +0800

    first version of the objective function

--- python_dual_gear/objective_function.py
+++ python_dual_gear/objective_function.py
@@ -3,6 +3,7 @@ import numpy as np
 from dtw import dtw
 import math
 import matplotlib.pyplot as plt
+from typing import Union
 
 
 def calculate_area(points):
@@ -39,7 +40,7 @@ def triangle_area_representation(contour: np.ndarray, sample_count: int) -> np.n
 def tar_to_distance_matrix(tar_a: np.ndarray, tar_b: np.ndarray) -> np.ndarray:
     assert tar_a.shape == tar_b.shape
     ts = tar_a.shape[1]
-    answer = np.empty(tar_a.shape[0], tar_b.shape[0])
+    answer = np.empty((tar_a.shape[0], tar_b.shape[0]), dtype=float)
     for i in range(answer.shape[0]):
         for j in range(answer.shape[1]):
             distance_sum = 0
@@ -60,6 +61,28 @@ def dtw_distance(distance_matrix: np.ndarray, offset: int) -> float:
         return distance_matrix[index_a, index_b]
 
     return dtw(distance_matrix.shape, distance)[0]
+
+
+def tar_distance(tar_a: np.ndarray, tar_b: np.ndarray) -> float:
+    assert tar_a.shape == tar_b.shape
+    distance_matrix = tar_to_distance_matrix(tar_a, tar_b)
+    return min([dtw_distance(distance_matrix, offset) for offset in range(tar_a.shape[0])])
+
+
+def shape_difference_rating(contour_a: np.ndarray, contour_b: np.ndarray,
+                            sample_rate: Union[int, None] = None) -> float:
+    """
+    calculate the shape difference level according to TAR function and DSW
+    :param contour_a: the contour A array([(x,y)]) in counterclockwise direction
+    :param contour_b: the contour B array([(x,y)]) in counterclockwise direction
+    :param sample_rate: the re-sampled rate used in the calculation, None for choosing the maximum of input
+    :return: TAR DSW-difference
+    """
+    if sample_rate is None:
+        sample_rate = max(contour_a.shape[0], contour_b.shape[0])
+    tar_a = triangle_area_representation(contour_a, sample_rate)
+    tar_b = triangle_area_representation(contour_b, sample_rate)
+    return tar_distance(tar_a, tar_b)
 
 
 if __name__ == '__main__':
@@ -86,3 +109,6 @@ if __name__ == '__main__':
         tar = tar[:, 0]
         subplot.plot(range(len(tar)), tar, color='blue')
     plt.axis('equal')
+    distances = np.array(
+        [[shape_difference_rating(contour_a, contour_b, 48) for contour_b in contours] for contour_a in contours])
+    print(distances)
