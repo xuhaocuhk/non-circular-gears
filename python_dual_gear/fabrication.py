35356576f355b164d6544efc6a1710d8f58f963c
Fu Tianwen <kevin.futianwen@gmail.com>
2019-07-26 22:19:41 +0800

    half fabrication Friday test case

--- python_dual_gear/fabrication.py
+++ python_dual_gear/fabrication.py
@@ -1,6 +1,11 @@
 """
 functions related to 3D modelling and fabrication
 """
+from debug_util import MyDebugger
+import numpy as np
+import openmesh
+from shapely.geometry import Polygon
+from shapely.ops import triangulate
 
 
 def generate_2d_obj(debugger, filename, points):
@@ -15,6 +20,30 @@ def generate_2d_obj(debugger, filename, points):
         for i in range(len(points)):
             print(i + 1, end=' ', file=file)
         print(1, file=file)
+
+
+def generate_3d_mesh(debugger: MyDebugger, filename: str, contour: np.ndarray, thickness: float):
+    """
+    generate a 3D mesh of the given contour with the given thickness
+    :param debugger: the debugger to provide directory for obj to be stored
+    :param filename: filename (excluding the extension)
+    :param contour: the contour to create 3d object with
+    :param thickness: the thickness of 3d object mesh
+    :return:
+    """
+    destination = debugger.file_path(filename)
+    mesh = openmesh.TriMesh()
+    lower_plane = [mesh.add_vertex((np.array(*point, 0) for point in contour))]  # TODO: make this more graceful
+    upper_plane = [mesh.add_vertex((np.array(*point, thickness) for point in contour))]
+    contour_poly = Polygon(contour)
+    point_to_vertex = {
+        tuple(point): (lower_vertex, upper_vertex)
+        for point, lower_vertex, upper_vertex in zip(contour, lower_plane, upper_plane)
+    }
+    triangles = triangulate(contour_poly)
+    for triangle in triangles:
+        *points, _ = triangle
+        mesh.add_face(zip(*[point_to_vertex[point] for point in points]))  # TODO: is this correct?
 
 
 def generate_printable_spline(debugger, contour1, contour2, center_dist, target_dist=100):
