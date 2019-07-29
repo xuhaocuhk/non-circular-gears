ec93f7f1513f0405e65f26ca40ce9503ad27e4c2
Fu Tianwen <kevin.futianwen@gmail.com>
2019-07-29 22:03:50 +0800

    removed matlab and opengl files which are not to be used

--- opengl/GearFunction.cpp
+++
@@ -1,8 +0,0 @@
-#include "GearFunction.h"
-#include "utility.h"
-
-const GLfloat CenterEllipseGearFunction::operator()(GLfloat theta) const {
-    const auto &x = b * cos(theta);
-    const auto &y = a * sin(theta);
-    return a * b / sqrt(x * x + y * y);
-}
