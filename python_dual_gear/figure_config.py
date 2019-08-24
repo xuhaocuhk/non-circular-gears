d406583961262bbad97b81e86cc77925cf972b89
Fu Tianwen <kevin.futianwen@gmail.com>
2019-08-24 16:52:10 +0800

    final figure config, withdrawing matplotlib

--- python_dual_gear/figure_config.py
+++ python_dual_gear/figure_config.py
@@ -10,17 +10,17 @@ axis_range = {
 }
 scatter_point = {
     'size': 0.05,
-    'color': (1.0, 1.0, 1.0),
-    'edge': (0.498, 0.498, 0.498)
+    'color': (1.0, 1.0, 1.0, 1.0),
+    'edge': (0.498, 0.498, 0.498, 1.0)
 }
 input_shapes = {
-    'drive_face': (0.8, 0.8, 0.8),
-    'drive_edge': (0.498, 0.498, 0.498),
-    'driven_face': (1.0, 0.0, 0.0),
-    'driven_edge': (0.498, 0.498, 0.498),
+    'drive_face': (0.8, 0.8, 0.8, 1),
+    'drive_edge': (0.498, 0.498, 0.498, 1),
+    'driven_face': (1.0, 0.0, 0.0, 1),
+    'driven_edge': (0.498, 0.498, 0.498, 1),
 }
 math_shapes = {
-    key: tuple((item / 255 for item in value))
+    key: tuple((item / 255 for item in value)) + (1,)
     for key, value in {
         'drive_face': (183, 208, 207),
         'drive_edge': (127, 127, 127),
@@ -29,7 +29,7 @@ math_shapes = {
     }.items()
 }
 carve_shapes = {
-    key: tuple((item / 255 for item in value))
+    key: tuple((item / 255 for item in value)) + (1,)
     for key, value in {
         'drive_face': (165, 189, 225),
         'drive_edge': (127, 127, 127),
