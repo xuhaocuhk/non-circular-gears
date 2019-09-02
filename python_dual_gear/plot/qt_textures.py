c89c4df6c69722b1adcb468188bbcaf91a2ae8ec
Fu <kevin.futianwen@gmail.com>
2019-09-02 10:37:04 +0800

    aligning

--- python_dual_gear/plot/qt_textures.py
+++ python_dual_gear/plot/qt_textures.py
@@ -2,6 +2,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 import yaml
 from typing import Tuple
+import numpy as np
+import logging
+from shapely.geometry import Polygon
+
+logger = logging.getLogger(__name__)
+
+Point_T = Tuple[float, float]
+Radian_T = float
 
 
 def load_image(image_file: str) -> QtGui.QImage:
@@ -12,6 +20,32 @@ def load_image(image_file: str) -> QtGui.QImage:
     """
     assert os.path.isfile(image_file)
     return QtGui.QImage(image_file)
+
+
+def align_center(contour: np.ndarray, center: Point_T, image: QtGui.QImage) -> Point_T:
+    """
+    align the center of a contour to the center in the image
+    :param contour: the contour
+    :param center: center of the contour
+    :param image: the image to align center in
+    :return: the corresponding center on the image
+    """
+    min_x, min_y, max_x, max_y = Polygon(contour).bounds
+    x, y = center
+    x_ratio = (x - min_x) / (max_x - min_x)
+    y_ratio = (y - min_y) / (max_y - min_y)
+    w, h = image.width(), image.height()
+    return x_ratio * w, y_ratio * h
+
+
+def rotate_painter(painter: QtGui.QPainter, center: Point_T, rotation_angle: Radian_T) -> QtGui.QPainter:
+    """
+    rotate a painter counterclockwise with respect to center point
+    :param painter: painter to be rotated
+    :param center: center of rotation
+    :param rotation_angle: angle in radians
+    :return: rotated painter
+    """
 
 
 class Texture:
