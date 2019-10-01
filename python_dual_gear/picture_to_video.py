79e42d5dda44f97a856cdd93c6f2389ce111cad3
Fu Tianwen <kevin.futianwen@gmail.com>
2019-10-01 13:54:50 +0800

    logging update

--- python_dual_gear/picture_to_video.py
+++ python_dual_gear/picture_to_video.py
@@ -14,13 +14,14 @@ models_root_dir = r'C:\Projects\gears\python_dual_gear\debug\pending'
 # names not treated as a model
 ignored_names = ['picture_to_video.py', '.DS_Store']
 # folder to be searched inside each model
-folder_name = 'cut_rotate'
+folder_name = 'png'
 # file prefix of the photos
-prefix = 'after_cut_'
+prefix = ''
 # also attach a reversed version
 attach_reverse = True
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 models_root_dir = os.path.abspath(models_root_dir)
 models = os.listdir(models_root_dir)
@@ -45,7 +46,7 @@ for model in models:
     img_array = []
     for image_file in image_files:
         filename = os.path.abspath(os.path.join(folder, prefix + image_file + '.png'))
-        logging.info(f'Processed {filename}')
+        logger.info(f'Processed {filename}')
         img = cv2.imread(filename)
         height, width, layers = img.shape
         size = (width, height)
@@ -54,6 +55,7 @@ for model in models:
     if attach_reverse:
         rev_img_array = deepcopy(img_array)[::-1]
         img_array = img_array + rev_img_array
+        logger.info('reverse data attached')
 
     out = cv2.VideoWriter(os.path.join(models_root_dir, video_name), cv2.VideoWriter_fourcc(*'mp4v'),
                           len(img_array) / video_length_sec, size)
